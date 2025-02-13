4c98b826bfc74a2d8b1d5020e9b1cd6722a90771
agabak <agaba_k@hotmail.com>
2025-02-12 20:19:46 -0600

    remove the comments

--- Blazor.AzureCosmosDb.Demo/Program.cs
+++ Blazor.AzureCosmosDb.Demo/Program.cs
@@ -1,6 +1,4 @@
 using Blazor.AzureCosmosDb.Demo.Data;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +13,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
